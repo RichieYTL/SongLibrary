ddfba33e509f3e992854282dbb60ed264fa7e38f
Liam <Liam@25.218.161.71>
2017-02-04 23:24:25 -0500

    First Commit

--- SongLibrary/src/SongLibrary/List.java
+++ SongLibrary/src/SongLibrary/List.java
@@ -2,4 +2,41 @@ package SongLibrary;
 
 public class List {
 
+	static Song top;
+	static Song selection;
+	static int numSongs;
+	
+	public List(){
+		numSongs = 0;
+		selection = top;
+	}
+	
+	public static boolean searchList(String Song, String Artist)
+	{
+	if (top == null){
+		return false;
+	}	
+	
+	
+	Song current = top;
+	
+	for (int i = 0; i <numSongs; i++)
+	{
+		if (current.Title.equals(Song) && current.Artist.equals(Artist))
+		{
+			return true;
+			}
+		}
+		
+		return false;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }

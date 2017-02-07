86ce96832bd7c7aff434cf2686e843afbaee71bd
LJOBrien <liam.j.obrien12@gmail.com>
2017-02-07 10:58:50 -0500

    Add files via upload

---
+++ SongLibrary/src/SongLibrary/musicController.java
@@ -0,0 +1,14 @@
+package SongLibrary;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class musicController {
+	
+	@FXML Button Add;
+	@FXML Button Edit;
+	@FXML Button Delete;
+
+}

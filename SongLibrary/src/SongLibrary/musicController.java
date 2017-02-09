42d29bb2826c25b6a5a56e05651167930294f31a
LJOBrien <liam.j.obrien12@gmail.com>
2017-02-09 16:31:46 -0500

    Update musicController.java

--- SongLibrary/src/SongLibrary/musicController.java
+++ SongLibrary/src/SongLibrary/musicController.java
@@ -1,14 +1,34 @@
 package SongLibrary;
 
-import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
+import java.net.URL;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+
 
 public class musicController {
 	
-	@FXML Button Add;
-	@FXML Button Edit;
-	@FXML Button Delete;
+	@FXML
+	Button addSong;
+	@FXML
+	Button editSong;
+	@FXML
+	Button deleteSong;
+	@FXML
+	ListView<Song> listView;
+	
+	public void start(Stage mainStage) {
+		
+	}
+	
+	
+	
 
 }
